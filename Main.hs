30ef62e44c3667134e4abbe8839a05e7881b99be
joranvar <bart.post@gmail.com>
2018-04-25 20:30:36 +0200

    Add statuses from stdin

--- Main.hs
+++ Main.hs
@@ -1,26 +1,69 @@
-{-# LANGUAGE OverloadedStrings, DeriveAnyClass, DeriveGeneric #-}
-module Main where
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving, DeriveGeneric,
+  PartialTypeSignatures #-}
+module Main (main) where
 
-import Data.Maybe (fromJust)
-import Data.Time (UTCTime, parseTimeM, defaultTimeLocale)
-import Data.Text (Text)
+import Control.Concurrent (forkIO)
+import Control.Concurrent.STM (TVar, newTVarIO, readTVarIO, atomically, modifyTVar')
+import Control.Monad (forever)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (MonadReader, ReaderT, runReaderT, ask, lift)
 import Data.Aeson (ToJSON)
+import Data.Monoid ((<>))
+import Data.Time (UTCTime, getCurrentTime)
+import Data.Text.Lazy (Text)
+import Data.String (fromString)
 import GHC.Generics (Generic)
-import Web.Scotty (scotty, get, json)
+import System.IO (BufferMode(NoBuffering), hSetBuffering, stdout)
+import Web.Scotty.Trans (ActionT, scottyT, get, json)
 
 data Status = Status
     { label :: Text
     , since :: UTCTime
     , contents :: Text
-    } deriving (Generic, ToJSON)
+    } deriving (Generic)
+instance ToJSON Status
 
-statuses :: [Status]
-statuses =
-    [ Status
-          "main"
-          (fromJust $ parseTimeM True defaultTimeLocale "%FT%X" "2018-04-25T15:37:00")
-          "working with scotty"
-    ]
+newtype AppState = AppState
+    { status :: [Status]
+    }
+newtype WebM a = WebM
+    { runWebM :: ReaderT (TVar AppState) IO a
+    } deriving ( Functor
+               , Applicative
+               , Monad
+               , MonadIO
+               , MonadReader (TVar AppState)
+               )
+
+gets :: (AppState -> b) -> WebM b
+gets f = f <$> (ask >>= liftIO . readTVarIO)
+
+modify :: (AppState -> AppState) -> WebM ()
+modify f = ask >>= liftIO . atomically . flip modifyTVar' f
 
 main :: IO ()
-main = scotty 13337 $ get "/finger" $ json statuses
+main = do
+    hSetBuffering stdout NoBuffering
+    state <- newTVarIO $ AppState []
+    _ <-
+        forkIO $
+        scottyT
+            13337
+            (\m -> runReaderT (runWebM m) state) $
+        get "/finger" $ do
+            currentStatus <- lift $ gets status
+            json currentStatus
+                :: ActionT Text WebM ()
+    forever $ do
+        putStr "Set a new label: "
+        l <- getLine
+        putStr $ "Set a new " <> l <> ": "
+        content <- getLine
+        now <- getCurrentTime
+        atomically $
+            modifyTVar'
+                state
+                (\s ->
+                     AppState $
+                     Status (fromString l) now (fromString content) :
+                     status s)
