002f75929dc347e06c52161e71a762c3396148a8
joranvar <bart.post@gmail.com>
2018-04-25 15:25:46 +0200

    Initial import

---
+++ nix/default.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+let
+  finger = pkgs.haskellPackages.callCabal2nix "finger" (pkgs.stdenv.mkDerivation rec {
+  name = "finger-src";
+  cabal = ../finger.cabal;
+  srcs = [ ../LICENSE ../Main.hs ];
+  phases = ["unpackPhase"];
+  unpackPhase = ''
+    set -Eux
+    mkdir $out
+    cp ${cabal} $out/finger.cabal
+    ${pkgs.lib.concatStrings (map (f: "cp ${f} $out/${baseNameOf f}\n") srcs)}
+    set +Eux
+  '';
+  }) {};
+in {
+  inherit finger;
+}
